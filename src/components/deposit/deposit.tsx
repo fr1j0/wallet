3665550e42d137bdd17fd2406e03cf1c9a2a9714
fr1j0 <fercasti@gmail.com>
2021-07-10 15:30:00 +0400

    deposit layout

--- src/components/deposit/deposit.tsx
+++ src/components/deposit/deposit.tsx
@@ -1,12 +1,36 @@
 import React from "react";
-import Input from "../input";
 import Pod from "../pod";
 
 const Deposit = () => {
   return (
     <Pod title="Deposit" description="Select currency and payment method">
-      <Input />
-      <Input />
+      <div className="w-full rounded-2xl py-3 px-4 bg-blue-200 shadow-inner">
+        <div className="w-full text-sm mb-3">Amount</div>
+        <div className="w-full text-4xl font-bold text-gray-600 flex justify-between">
+          <input
+            type="number"
+            min="1"
+            max="100000"
+            className="appearance-none bg-transparent border-none shadow-none w-4/5 py-2 pr-3 text-gray-700 leading-tight focus:outline-none"
+            placeholder="0.0"
+          />
+          <select
+            name="currency"
+            className="text-lg bg-transparent focus:outline-none"
+          >
+            <option value="" disabled selected>
+              currency
+            </option>
+            <option value="eur">EUR</option>
+            <option value="usd">USD</option>
+            <option value="aed">AED</option>
+            <option value="chf">CHF</option>
+          </select>
+        </div>
+      </div>
+      <button className="w-full mt-4 bg-blue-600 text-sm text-white p-4 rounded-2xl font-semibold">
+        Deposit
+      </button>
     </Pod>
   );
 };
--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -2,8 +2,11 @@ import React from "react";
 
 const Header = () => {
   return (
-    <header className="fixed w-full h-16 border-b-2 bg-white flex items-center px-4">
-      <div>Wallet</div>
+    <header className="fixed w-full h-16 font-bold border-b-2 bg-white flex items-center pl-16 pr-4 py-4">
+      <div className="">
+        <span className="text-gray-700">dino</span>
+        <span className="text-gray-300">wallet</span>
+      </div>
     </header>
   );
 };
